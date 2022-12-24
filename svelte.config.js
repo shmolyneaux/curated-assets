253893bc4323faad71677decbbd98efed56a7846
Stephen Molyneaux <stephen@molyneaux.ca>
2022-12-24 14:31:54 -0500

    Updated to sveltekit 1.0.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// TODO: set up static adapter
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
-		prerender: {
-			default: true,
-		},
-		adapter: adapter({
-			fallback: 'index.html',
-		}),
+		adapter: adapter()
 	}
 };
 
